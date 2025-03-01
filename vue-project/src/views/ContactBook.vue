6986d049c5ab578278132fc1d6978dbf3cff7238
phong231004 <huynhthanhphong23102004@gmail.com>
2025-03-01 16:12:48 +0700

    Tạo trang cập nhật liên hệ

--- vue-project/src/views/ContactBook.vue
+++ vue-project/src/views/ContactBook.vue
@@ -5,7 +5,7 @@
     </div>
     <div class="mt-3 col-md-6">
       <h4>
-        Dani bg
+        Danh Bạ
         <i class="fas fa-address-book"></i>
       </h4>
       <ContactList
@@ -38,6 +38,16 @@
           <i class="fas fa-address-card"></i>
         </h4>
         <ContactCard :contact="activeContact" />
+        <router-link
+          :to="{
+            name: 'contact.edit',
+            params: { id: activeContact._id },
+          }"
+        >
+          <span class="mt-2 badge badge-warning">
+            <i class="fas fa-edit"></i> Hiệu chỉnh
+          </span>
+        </router-link>
       </div>
     </div>
   </div>
