7182032063ebcad05b84e8972a6d288236e208d3
phong231004 <huynhthanhphong23102004@gmail.com>
2025-03-01 15:28:47 +0700

    Hiển thị các contact lấy từ server

--- vue-project/src/main.js
+++ vue-project/src/main.js
@@ -1,6 +1,10 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import "bootstrap/dist/css/bootstrap.min.css"; // Import Bootstrap CSS
+import "@fortawesome/fontawesome-free"; // Import FontAwesome
+import router from './router';
+import './assets/main.css'; // Import file CSS của bạn
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App)
+  .use(router)
+  .mount('#app');
